caa5e0dfeb5c5b5f31e5a48f41f6525bd96753b1
jorgemartinez <www.jorge_a14@hotmail.com>
2021-09-02 12:16:18 -0500

    se agrega metod-set ejemplo

---
+++ 003-practicas/metod-sets/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type persona struct {
+	nombre string
+}
+
+func (p *persona) hablar() {
+	fmt.Println("Hola soy", p.nombre)
+}
+
+type humano interface {
+	hablar()
+}
+
+func diAlgo(h humano) {
+	h.hablar()
+}
+func main() {
+	p1 := persona{
+		nombre: "Jorge",
+	}
+	diAlgo(&p1)
+	p1.hablar()
+}
